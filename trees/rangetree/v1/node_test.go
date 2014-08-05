7bf2548203b5ae13a5150629983af53079995c3b
Dustin Hiatt <dustin.hiatt@webfilings.com>
2014-08-05 14:41:57 -0500

    Unit tests around the shifting functions.

--- trees/rangetree/v1/node_test.go
+++ trees/rangetree/v1/node_test.go
@@ -24,3 +24,79 @@ func TestReverse(t *testing.T) {
 		t.Errorf(`Expected: %+v, received: %+v`, expected, nodes)
 	}
 }
+
+func TestShiftLeft(t *testing.T) {
+	p := newOrderedList(2)
+	p1 := newPoint(1, 1)
+	p.insert(p1)
+	n := &node{
+		value: 1,
+		p:     p,
+	}
+
+	tree := New(2)
+
+	shiftLeft(n, 2, tree)
+
+	if n.value != 2 {
+		t.Errorf(`Expected: %d, received: %d`, 2, n.value)
+	}
+
+	if n.left.value != 1 {
+		t.Errorf(`Expected: %d, received: %d`, 1, n.left.value)
+	}
+
+	if n.right.value != 2 {
+		t.Errorf(`Expected: %d, received: %d`, 2, n.right.value)
+	}
+
+	if n.p.len() != 1 {
+		t.Errorf(`Expected len: %d, received: %d`, 1, n.p.len())
+	}
+
+	if n.right.p.len() != 0 {
+		t.Errorf(`Expected len: %d, received: %d`, 0, n.right.p.len())
+	}
+
+	if n.left.p.len() != 1 {
+		t.Errorf(`Expected len: %d, received: %d`, 1, n.left.p.len())
+	}
+}
+
+func TestShiftRight(t *testing.T) {
+	p := newOrderedList(2)
+	p1 := newPoint(1, 1)
+	p.insert(p1)
+	n := &node{
+		value: 1,
+		p:     p,
+	}
+
+	tree := New(2)
+
+	shiftRight(n, 0, tree)
+
+	if n.value != 1 {
+		t.Errorf(`Expected: %d, received: %d`, 1, n.value)
+	}
+
+	if n.left.value != 0 {
+		t.Errorf(`Expected: %d, received: %d`, 0, n.left.value)
+	}
+
+	if n.right.value != 1 {
+		t.Errorf(`Expected: %d, received: %d`, 1, n.right.value)
+	}
+
+	if n.p.len() != 1 {
+		t.Errorf(`Expected len: %d, received: %d`, 1, n.p.len())
+	}
+
+	if n.right.p.len() != 1 {
+		t.Errorf(`Expected len: %d, received: %d`, 1, n.right.p.len())
+	}
+
+	if n.left.p.len() != 0 {
+		t.Errorf(`Expected len: %d, received: %d`, 0, n.left.p.len())
+	}
+}
